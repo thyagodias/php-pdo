564ce10b319b2ae343413df69586d5f50d500fd2
Thyago Dias <thyagojuniorrochadias@gmail.com>
2020-09-02 16:20:44 -0400

    feat: adiciona configurações iniciais

---
+++ src/Domain/Model/Student.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Alura\Pdo\Domain\Model;
+
+use DateTimeImmutable;
+use DateTimeInterface;
+
+class Student
+{
+    private ?int $id;
+    private string $name;
+    private DateTimeInterface $birthDate;
+
+    public function __construct(?int $id, string $name, DateTimeInterface $birthDate)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->birthDate = $birthDate;
+    }
+
+    public function id(): ?int
+    {
+        return $this->id;
+    }
+
+    public function name(): string
+    {
+        return $this->name;
+    }
+
+    public function birthDate(): DateTimeInterface
+    {
+        return $this->birthDate;
+    }
+
+    public function age(): int
+    {
+        return $this->birthDate
+            ->diff(new DateTimeImmutable())
+            ->y;
+    }
+}
+
+function soma($numbers) {
+    $soma = 0;
+    foreach ($numbers as $number) {
+        $soma += $number;
+    }
+    echo "A soma foi de $soma";
+}
+
+soma([1,2,3]);
